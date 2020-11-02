5031529a74dba919e558b52467309a215feb42e9
hknaln <hkn.aln67@gmail.com>
2020-11-02 12:06:25 +0300

    first commit

---
+++ ders-06-10-2020/01-Animal-Years - Project/script.js
@@ -0,0 +1,27 @@
+//my age
+let myAge=32;
+//eraly years
+let earlyYears;
+earlyYears="2";
+
+
+console.log(earlyYears*10.5);
+
+let laterYears = myAge - 2; //myAge den 2 çıkarıldı ve laterYears değişkenine atandı
+console.log(laterYears * 4);
+
+
+
+let myAgeInDogYears= laterYears+earlyYears; //earlyYeras+laterYears
+
+
+
+
+let myName = 'Hakan'.toLowerCase();
+//myName = myName.toLowerCase();  // Bu metot stringdeki tüm harfleri küçük yazar
+console.log(`My name is ${myName}. I am ${myAge} years old in human years which is ${myAgeInDogYears} years old in dog years.`);
+
+
+
+
+
